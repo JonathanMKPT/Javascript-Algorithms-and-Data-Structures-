63e54e810a9cb04284fbde6a49330fc19d739713
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-07-24 09:15:25 -0400

    Pointer representation pattern for arrays, will update as my knowledge increases

---
+++ pointerPattern1.js
@@ -0,0 +1,10 @@
+//return the sum of an array that returns 0 when added togeteher, time comp = O(N^2) space comp = O(1)
+function sumZero(arr){
+    for(let i = 0; i < arr.length; i++){
+        for(let j = i + 1; j < arr.length; j++){
+            if(arr[i] + arr[j] === 0){
+                return [arr[i], arr[j]];
+            }
+        }
+    }
+}
