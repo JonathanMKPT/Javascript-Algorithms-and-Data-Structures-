676526d15d8057d51d321c1be1c3c4778bd993b4
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-08-26 03:35:13 -0400

    Demonstrated a helper method to find odd values in an array

---
+++ recursive_helper.js
@@ -0,0 +1,20 @@
+function collectOdds(num){
+	let results = [];
+	
+	function helpr(helperInput){
+		if(helperInput.length === 0){
+			return;
+		}
+		
+		if(helperInput[0] % 2 !== 0){
+			result.push(helperInput[0])
+		}
+		helper(helperInput.slice(1))
+	}
+	helper(arr)
+	
+	return result;
+}
+
+collectOdds([1,2,3,4,5,6,7,8])
+//expected output 3,5,7
