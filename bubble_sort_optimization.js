75a6d3ecd68acbff631417f64875b1ea48662ab5
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-08-28 09:01:37 -0400

    Added ES6 syntax in a block comment

--- bubble_sort_optimization.js
+++ bubble_sort_optimization.js
@@ -18,4 +18,19 @@ function bubbleSort(arr){
 	return arr;
 }
 
+/*ES6 Syntax 
+function bubbleSort(arr){
+	const swap = (arr,idx1, idx2) => {
+		[arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+	};
+	
+	for(let i = arr.length; i > 0; i--){
+		for(let j = 0; j < i - 1; j++) {
+			if(arr[j] > arr[j + 1]){
+				swap(arr, j, j + 1);
+			}
+		}
+	}
+} */
+
 bubbleSort([4,6,12,8,9,25,47,34,43]);
