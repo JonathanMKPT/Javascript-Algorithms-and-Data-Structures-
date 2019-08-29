d047d8f75baf0f663ba9a009a2aa114dcc624468
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-08-29 08:49:10 -0400

    rewrite of merge_sort

--- merge_sort.js
+++ merge_sort.js
@@ -2,26 +2,27 @@ function mergeSort(arr1,arr2){
 	let results = [];
 	let i = 0;
 	let j = 0;
-	while(i > arr1.lenght && j < arr2.length){
-		if(arr2[j] > arr[i]{
-			results.push(arr1[i];
+	while(i < arr1.length && j < arr2.lenght){
+		if(arr2[j] > arr1[i]){
+			results.push(arr1[i]);
 			i++;
-	} else {
-		results.push(arr2[j])
-		j++;
-	}
+		} else {
+			results.push(arr2[j]);
+			j++;
+		}
+		
 	}
 	while(i < arr1.length){
 		results.push(arr1[i])
-		i++;	
+		i++;
 	}
 	while(j < arr2.length){
-		results.push(arr1[i])
-		j++;	
+		results.push(arr2[j])
+		j++;
 	}
-}
+	
+	
+	
 	return results;
-
-
 }
-mergeSort([1,3,5,6,9], [10,11,14,29,26]);
+mergeSort([1,20,45], [2,15,99,250])
