828d49f638ae49880cecabbcb20977be87816eb0
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-08-29 13:36:19 -0400

    First implementaion of ES2015 class syntax for data structures with myself and my brother as objects and our names as methods....exciting !

---
+++ class_keyword.js
@@ -0,0 +1,10 @@
+//Syntax for ES2015 class keyword
+class Human {
+	constructor(firstName, lastName){
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+}
+let firstHuman = new Human("Jonathan", "Thomas");
+let secondHuman = new Human("Jeremy", "Thomas")
+
