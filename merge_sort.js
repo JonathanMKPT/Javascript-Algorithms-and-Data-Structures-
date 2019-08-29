6f1e461afe6206e8a1fa105e84455033ff98292d
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-08-29 09:19:23 -0400

    Added mergeSort function that recursively calls the left and right to return a sorted array, THANKS VON NEUMANgit statusgit statusgit status

--- merge_sort.js
+++ merge_sort.js
@@ -1,8 +1,8 @@
-function mergeSort(arr1,arr2){
+function merge(arr1,arr2){
 	let results = [];
 	let i = 0;
 	let j = 0;
-	while(i < arr1.length && j < arr2.lenght){
+	while(i < arr1.length && j < arr2.length){
 		if(arr2[j] > arr1[i]){
 			results.push(arr1[i]);
 			i++;
@@ -20,9 +20,15 @@ function mergeSort(arr1,arr2){
 		results.push(arr2[j])
 		j++;
 	}
-	
-	
-	
 	return results;
 }
-mergeSort([1,20,45], [2,15,99,250])
+function mergeSort(arr){
+	if(arr.length <= 1) return arr;
+	let mid = Math.floor(arr.length/2);
+	let left = mergeSort(arr.slice(0,mid));
+	let right = mergeSort(arr.slice(mid));
+	return merge(left,right);
+}
+
+mergeSort([10,23,56,32,1,5,9])
+
