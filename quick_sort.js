da3822732a882aae3bace02c23a5f44f3138a9ff
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-08-29 11:25:34 -0400

    Implementaion of quicksort with ES6 swap functionality...also i forgot to use a + sign that's why it wasn't running....HOORAY!

--- quick_sort.js
+++ quick_sort.js
@@ -1,21 +1,28 @@
-function pivot(arr, start=0, end=arr.length-1){
-	function swap(arr,i,j){
-		var temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp
-	}
-	var pivot = arr[start];
-	var swapIndx = start;
+function pivot(arr, start = 0, end = arr.length-1){
+	const swap =(arr, idx1, idx2) => {
+		[arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+	};
+	let pivot = arr[start];
+	let swapIndx = start;
 	
-	for(var i = start + 1; i < arr.length; i++){
+	for(let i = start + 1; i <= end; i++){
 		if(pivot > arr[i]){
 			swapIndx++;
-			swap(arr,swapIdx,i)
+			swap(arr,swapIndx,i)
 			
 		}
 	}
-	swap(arr,start,swapIdx)
-	return swapIdx;
+	swap(arr,start,swapIndx)
+	return swapIndx;
 }
 
-pivot([4,8,2,1,5,7,6,3])
+function quickSort(arr, left = 0, right = arr.length -1){
+	if(left < right){
+		let pivotIndex = pivot(arr, left, right)
+		quickSort(arr,left,pivotIndex-1);
+		quickSort(arr,pivotIndex+1,right);
+	}
+	return arr;
+}
+
+quickSort([4,8,2,1,5,7,6,3])
