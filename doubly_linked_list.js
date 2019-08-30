eb463320ecd2c86f20b94e794fe5fae1ecdcb5f0
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-08-30 16:43:14 -0400

    Added unshift() method

--- doubly_linked_list.js
+++ doubly_linked_list.js
@@ -57,6 +57,20 @@ class DoublyLinkedList {
 				
 				
 		}
+		unshift(val){
+			var newNode = new Node(val)
+			if(this.length === 0){
+				this.head = newNode 
+				this.tail = newNode 
+			} else {
+				this.head.prev = newNode
+				newNode.next = this.head
+				this.head = newNode
+			}
+			this.length++
+			return this
+				
+		}
 		
 	
 }
