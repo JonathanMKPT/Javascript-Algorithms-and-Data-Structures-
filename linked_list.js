ba7b549703b8da1bb1729b2918bfb8d66dec4017
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-08-30 14:04:12 -0400

    Added remove() method

--- linked_list.js
+++ linked_list.js
@@ -96,6 +96,18 @@ class SinglyLinkedList {
 			return true
 			  
 	}
+	remove(index){
+		if(index < 0 || index >= this.length) return undefined
+		if(index === 0) return this.shift()
+		if(index === this.length -1) return this.pop()
+		var previousNode = this.get(index - 1)
+		var removed = previousNode.next
+		previousNode.next = removed.next
+		this.length--
+		return removed
+		
+			
+	}
 
 	
 	
