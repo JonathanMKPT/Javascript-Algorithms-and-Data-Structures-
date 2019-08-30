d161fb935aa12099cc83955cf82a0e65c69fb5dd
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-08-30 10:35:36 -0400

    Added a pop() method from follow along

--- linked_list.js
+++ linked_list.js
@@ -24,4 +24,17 @@ class SinglyLinkedList {
 		return this
 		
 	}
+	pop(){
+		if(!this.head) return undefined
+		var current = this.head
+		var newTail = current
+		while(current.next){
+			newTail = current
+			current = current.next
+		}
+		this.tail = newTail
+		this.tail.next = null
+		this.length--
+		return current
+	}
 }
