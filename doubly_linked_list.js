931a61d8be2852f4188681fe795d6c14b030d0a8
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-08-30 15:35:06 -0400

    Inital skeleton of doubly linked list

---
+++ doubly_linked_list.js
@@ -0,0 +1,17 @@
+class Node{
+	constructor(val,next,prev){
+		this.val = val
+		this.next = null
+		this.prev = null
+	}
+}
+
+class DoublyLinkedList {
+		constructor(){
+				this.head = null
+				this.tail = null
+				this.length = 0
+		}
+		
+	
+}
