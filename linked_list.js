a70fe2415d711c64f9af7b37e95fc1a411dc4f90
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-08-30 13:09:44 -0400

    Added a get() method to return index of the list

--- linked_list.js
+++ linked_list.js
@@ -65,5 +65,17 @@ class SinglyLinkedList {
 		return this
 		
 	}
+	get(index) {
+		if(index < 0 || index >= this.length) return null
+			var counter = 0
+			var current = this.head
+			while(counter !== index){
+					current = current.next
+					counter++
+				}
+				return current
+	}
+
+	
 	
 }
