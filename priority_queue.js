dbc5739679c772266fd9149d43c7f8c90b21313b
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-09-03 09:42:56 -0400

    Copied and replace logic from binary_heap and updated methods to priority_queue

--- priority_queue.js
+++ priority_queue.js
@@ -1,19 +1,76 @@
+class PriorityQueue{
+	constructor(){
+		this.values = []
+	}
+	enqueue(val,priority){
+		let newNode = new Node(val,priortiy)
+		this.values.push(newNode)
+		this.bubbleUp()
+	}
+	bubbleUp(){
+		let idx = this.values.length - 1
+		const element = this.values[idx]
+		while(idx > 0){
+			let parentIdx = Math.floor((idx -1 ) / 2)
+			let parent = this.values[parentIdx]
+			if(element.priority >= parent.priority) break
+			this.values[parentIdx] = element
+			this.values[idx] = parent
+			idx = parentIdx
+		}
+	}
+    dequeue(){
+        const min = this.values[0]
+        const end = this.values.pop()
+        this.values[0] = end
+        this.sinkDown()
+        
+        return max
+    }
+    sinkDown(){
+        let idx = 0
+        const length = this.values.length
+        const element = this.values[0]
+        while(true){
+            let leftChildIdx = 2 * idx + 1
+            let rightChildIdx = 2 * idx + 2
+            let leftChild, rightChild
+            let swap = null
+            
+            if(leftChildIdx < length){
+                leftChild = this.values[leftChildIdx]
+                if(leftChild.priority < element.priority){
+                    swap = leftChildIdx
+                }
+            }
+            if(rightChildIdx < length){
+                rightChild = this.values[rightChildIdx]
+                if(
+                    (swap === null && rightChild.priority < element.priority) || 
+                    (swap !== null && rightchild.priority < leftChild.priority)
+                ) {
+                   swap = rightChildIdx 
+                }
+            }
+            if(swap === null) break
+            this.values[idx] = this.values[swap]
+            this.values[swap] = element
+            idx = swap
+        }
+    }
+
+}
+
 class Node{
-	constructor(value,priority){
-		this.value = value
+	constructor(val,priority){
+		this.val = val
 		this.priority = priority
-		
 	}
 }
 
-class MinBinaryHeap{
-	constructor(){
-		
-	}
-	enqueue(value,priority){
-		
-	}
-	dequeue(){
-		
-	}
-}
+
+let ER = new PriorityQueue()
+ER.enqueue("common cold",5)
+ER.enqueue("GSW", 1)
+ER.enqueue("fracture",4)
+ER.enqueue("Alchol Poisoning",2)
