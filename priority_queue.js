3226ee3a9c26a6d723896558ca853ca0e3c974b1
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-09-03 09:23:18 -0400

    Started skeleton of priority_queue data structure on different machine

---
+++ priority_queue.js
@@ -0,0 +1,19 @@
+class Node{
+	constructor(value,priority){
+		this.value = value
+		this.priority = priority
+		
+	}
+}
+
+class MinBinaryHeap{
+	constructor(){
+		
+	}
+	enqueue(value,priority){
+		
+	}
+	dequeue(){
+		
+	}
+}
