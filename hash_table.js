735ba013c63cc23d4e2aab880d47cac9a8882398
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-09-04 06:47:44 -0400

    Added set and get methods, initatied HashTable class, moving to finalize keys and values next

--- hash_table.js
+++ hash_table.js
@@ -1,13 +1,36 @@
-function hash(key, arrLen){
-	let total = 0
-	let WEIRD_PRIME  - 31
-	for(let i = 0; i < Math.min(key.length, 100); i++){
-	//map "a" to "1" "b to 2, "c" to 3 etc.
-		let char - key[i]
-		let value = charCodeAt(0) - 96
-		total = (total * WEIRD_PRIME + value) % arrLen
+class HashTable{
+	constructor(sine=53){
+		this.keyMap = new Array(size)
 	}
-	return total
-		
+
+	_hash(key){
+		let total = 0
+		let WEIRD_PRIME  = 31
+		for(let i = 0; i < Math.min(key.length, 100); i++){
+		//map "a" to "1" "b to 2, "c" to 3 etc.
+			let char - key[i]
+			let value = char.charCodeAt(0) - 96
+			total = (total * WEIRD_PRIME + value) % this.keyMap.length
+		}
+		return total
+	}
+	set(key,value){
+		let index = this._hash(key)
+		if(!this.keyMap[index]){
+			this.keyMap[index] = []
+		}
+		this.keyMap[index].push([key,value])
+	}
+	get(key){
+	   let index  = this._hash(key)
+		if(this.keyMap[index]){
+			for(let i = 0; i < this.keyMap[index].length; i++){
+				if(this.keyMap[index][i][0] === key){
+					return this.keyMap[index][i]
+				}
+			}
+		}
+		return undefined
+	}	
 
 }
