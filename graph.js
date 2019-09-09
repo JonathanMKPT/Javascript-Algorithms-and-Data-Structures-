31eec6ffbf2c68360d364a8ad507994b09bcbcee
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-09-09 06:34:50 -0400

    Added recursive depth first search functionality and hard coded vertices and edges, will add iterative dfs next from follow along :)

--- graph.js
+++ graph.js
@@ -24,6 +24,41 @@ class Graph{
 		}
 		delete this.adjacencyList[vertex]
 	}
+	dfsRecursive(start){
+		const results = []
+		const visited = {}
+		const adjacencyList = this.adjacencyList
+		
+		(function dfs(vertex){
+			if(!vertex) return null
+			visited[vertex] = true
+			result.push(vertex)
+			adjacencyList[vertex].forEach(neighbour => {
+				if(!visisted[neighbour]){
+					return dfs(neighbour)
+				}
+			})
+			
+		})(start)
+		
+		return result
+	}
 	
 	
 }
+
+let g = new Graph
+
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+
+g.addEdge("A")
+g.addEdge("B")
+g.addEdge("C")
+g.addEdge("D")
+g.addEdge("E")
+g.addEdge("F")
