5469964be5b4e940858dd6404efe12e97d2d4d05
JonathanMKPT <jonathan.thomas251@gmail.com>
2019-09-10 08:55:55 -0400

    Added Dikjsta's Algorithim for finding the shortest past first, next will be a trfactor using binary heap

--- weightedGraph.js
+++ weightedGraph.js
@@ -27,8 +27,57 @@ class WeightedGraph{
 		this.adjacencyList[vertex1].push({node:vertex2, weight})
 		this.adjacencyList[vertex2].push({node:vertex1, weight})
 	}
-	
+	Dijkstra(start, finish){
+		const node = new Priority()
+		const distances = {}
+		const previous = {}
+		let path = [] // return at end
+		let smallest	
+		//build up initial state
+		for(let vertex in this.adjacencyList){
+			if(vertex === start){
+				distances[vertex] = 0
+				nodes.enqueue(vertex,0)
+			} else {
+				distances[vertex] = Infinity
+				nodes.enqueue(vertex, Infinity)
+			}
+			previous[vertex] = null
+		}
+		// as node needs to be visited
+		while(nodes.values.length){
+			smallest = node.dequeue().val
+			if(smallest === finish){
+				//build path to return at the end
+				while(previous[smallest]{
+					path.push(smallest)
+					smallest = previous[smallest]
+				}
+				break		
+			}
+			if(smallest || distances[smallest] !== Infinity){
+				//find neighbour node
+				for(let neighbour in this.adjacecnyList[smallest]){
+					let nextNode = this.adjacencyList(smallest[neighbour])
+					//calculate new distance to neighbouring node
+					let candidate = distances[smallest] + nextNode.weight
+					let nextNeighbour = nextNode.node
+					if(candidate < distances[nextNeighbour]){
+						//updating new smallest distance to neighbour
+						distances[nextNeighbour] = candidate
+						//updating previous 
+						previous[nextNeighbour] = smallest
+						//enqueue in priortiy queue with new priority
+						nodes.enqueue(nextNeighbour, candidate)
+					}
+				}
+			}
+		}
+		return path.concat(smallest).reverse()
+	}
 }
+
+
 var graph = new WeightedGraph
 graph.addVertex("A")
 graph.addVertex("B")
